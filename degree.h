d601769dbc21a60a9118f3048db9d7c386363ed2
Daniel Theroff <dtheroff@proton.me>
2024-11-22 14:53:57 -0600

    Creates Initial degree.h header file

---
+++ degree.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <string>
+#include <array>
+#include <iostream>
+
+
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+
+const std::string degreeProgramStrings[] = {"SECURITY", "NETWORK", "SOFTWARE"};
