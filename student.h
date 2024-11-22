495572fd254d8ce6725d3254139d75b18c727647
Daniel Theroff <dtheroff@proton.me>
2024-11-22 15:34:46 -0600

    Creates Student class and private variables

--- student.h
+++ student.h
@@ -3,3 +3,21 @@
 #include <string>
 #include <iostream>
 #include "degree.h"
+
+using std::string;
+
+
+class Student {
+private:
+    string student_ID;
+    string first_Name;
+    string last_Name;
+    string email_Address;
+
+    int student_Age;
+    int days_Completion[3];
+
+    DegreeProgram degree_Program;
+public:
+//WRITE NEXT
+};
