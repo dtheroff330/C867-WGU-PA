ed00490066b2a2c024ba1382d92d9d0de1f11b1b
Daniel Theroff <dtheroff@proton.me>
2024-11-22 15:09:42 -0600

    Adds comment explaining task

--- degree.h
+++ degree.h
@@ -10,3 +10,6 @@ enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
 
 
 const std::string degreeProgramStrings[] = {"SECURITY", "NETWORK", "SOFTWARE"};
+
+
+/*Task C - Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE. */
