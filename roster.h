70baef5cfaaf975b74db592d92bf3d3702ca3381
Daniel Theroff <dtheroff@proton.me>
2024-11-23 01:42:15 -0600

    Initial inclusions and creates roster class

---
+++ roster.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include "student.h"
+#include "degree.h"
+
+
+class Roster
+{
+public:
+/*fill with requirements from task E
+
+includes: classRosterArray, parse, add, remove, printAll, printAverageDaysInCourse
+printInvalidEmails, printByDegreeProgram,
+*/
+};
