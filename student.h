13fb710ba3b1ece5fd22ef92eedebef06be52bca
Daniel Theroff <dtheroff@proton.me>
2024-11-22 22:56:05 -0600

    Adds public methods for Student class

--- student.h
+++ student.h
@@ -19,5 +19,22 @@ private:
 
     DegreeProgram degree_Program;
 public:
-//WRITE NEXT
+    Student();
+    Student(string student_ID, string first_Name, string last_Name, string email_Address, int student_Age, int days_Completion[3], DegreeProgram degree_Program);
+    string getStudentID();
+    string getFirstName();
+    string getLastName();
+    string getEmailAddress();
+    int getStudentAge();
+    int getCourseCompletionDays();
+    DegreeProgram getDegreeProgram();
+    void setStudentID(string student_ID);
+    void setFirstName(string first_Name);
+    void setLastName(string last_Name);
+    void setEmailAddress(string email_Address);
+    void setAge(int student_Age);
+    void setCourseCompletionDays(int days1, int days2, int days3);
+    void setDegreeProgram(DegreeProgram degree_Program);
+    void print();
+
 };
