f27b7a5b345b4516e97f02b1628c34b987751399
Daniel Theroff <dtheroff@proton.me>
2024-11-24 15:22:25 -0600

    Creates new private definitions and variables

--- roster.h
+++ roster.h
@@ -11,10 +11,13 @@ using std::string;
 
 class Roster
 {
+private:
+    Student* classRosterArray[5];
+    int class_Size = 0;
+    int search_Index;
 public:
     Roster(int class_Size);
     ~Roster();
-    Student* classRosterArray[5];
     void parse(string student_Data);
     void add(string student_ID, string first_Name, string last_Name, string email_Address, int student_Age, int daysC1, int daysC2, int daysC3, DegreeProgram degree_Program);
     void remove(string student_ID);
@@ -22,6 +25,4 @@ public:
     void printAverageDaysInCourse(string student_ID);
     void printInvalidEmails();
     void printByDegreeProgram(DegreeProgram degree_Program);
-    int class_Size;
-    int index;
 };
