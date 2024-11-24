e3abb003dfe045c7a19076ae9478d0107643b7ea
Daniel Theroff <dtheroff@proton.me>
2024-11-24 13:54:15 -0600

    Adds Roster class function definitions

--- roster.h
+++ roster.h
@@ -5,13 +5,21 @@
 #include "student.h"
 #include "degree.h"
 
+using std::cout;
+using std::endl;
+using std::string;
 
 class Roster
 {
 public:
-/*fill with requirements from task E
-
-includes: classRosterArray, parse, add, remove, printAll, printAverageDaysInCourse
-printInvalidEmails, printByDegreeProgram,
-*/
+    Roster();
+    ~Roster();
+    Student* classRosterArray[5];
+    void parse(string student_Data);
+    void add(string student_ID, string first_Name, string last_Name, string email_Address, int student_Age, int daysC1, int daysC2, int daysC3, DegreeProgram degree_Program);
+    void remove(string student_ID);
+    void printAll();
+    void printAverageDaysInCourse(string student_ID);
+    void printInvalidEmails();
+    void printByDegreeProgram(DegreeProgram degree_Program);
 };
