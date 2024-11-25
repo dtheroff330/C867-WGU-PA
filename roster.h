3fb85484dd9366e376df46ded465eae6760a4564
Daniel Theroff <dtheroff@proton.me>
2024-11-25 13:45:01 -0600

    Slight formatting fixes

--- roster.h
+++ roster.h
@@ -20,9 +20,11 @@ private:
 public:
     Roster(int classSize);
     ~Roster();
+    
     void parse(string studentData);
     void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
     void remove(string studentID);
+
     void printAll();
     void printAverageDaysInCourse(string studentID);
     void printInvalidEmails();
