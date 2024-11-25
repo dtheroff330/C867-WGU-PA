23a84c26be25b2bbfd94dc6dc37b91e3b827c5ce
Daniel Theroff <dtheroff@proton.me>
2024-11-24 23:14:13 -0600

    Implements add function as well as blank versions of remaining functions for Roster

--- roster.cpp
+++ roster.cpp
@@ -83,4 +83,37 @@ void Roster::parse(string studentData)
     add(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeProgram);
 }
 
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram)
+{
+    classRosterArray[classSize] = new Student(studentID, firstName, lastName, emailAddress, age, (int[3]){daysInCourse1, daysInCourse2, daysInCourse3}, degreeProgram);
+}
+
+void Roster::remove(string studentID)
+{
+    //public void remove(string studentID)  that removes students from the roster by student ID. If the student ID does not exist, the function prints an error message indicating that the student was not found.
+}
+
+void Roster::printAll()
+{
+    //public void printAll() that prints a complete tab-separated list of student data in the provided format: A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security. The printAll() function should loop through all the students in classRosterArray and call the print() function for each student.
+}
+
+void printAverageDaysinCourse(string studentID)
+{
+    //public void printAverageDaysInCourse(string studentID)  that correctly prints a student’s average number of days in the three courses. The student is identified by the studentID parameter.
+}
+
+void printInvalidEmails()
+{
+    //public void printInvalidEmails() that verifies student email addresses and displays all invalid email addresses to the user.
+ 
+
+    //Note: A valid email should include an at sign ('@') and period ('.') and should not include a space (' ').
+}
+
+void printByDegreeProgram(DegreeProgram degreeProgram)
+{
+    //public void printByDegreeProgram(DegreeProgram degreeProgram) that prints out student information for a degree program specified by an enumerated type.
+}
+
 
