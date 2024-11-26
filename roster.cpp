74c93a9fefa84d813aa4bf251115a2e49c6eaea6
Daniel Theroff <dtheroff@proton.me>
2024-11-26 09:39:31 -0600

    Completely rewrites parse function to use stringstream

--- roster.cpp
+++ roster.cpp
@@ -5,11 +5,15 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <sstream>
 
 // Uses std namespace for specific things to avoid repetitive typing
 using std::cout;
 using std::endl;
 using std::string;
+using std::stoi;
+using std::stringstream;
+using std::getline;
 
 // Full constructor for roster class
 Roster::Roster(int classSize)
@@ -36,45 +40,41 @@ Roster::~Roster()
     }
 }
 
+/* 
+Uses stringstream to avoid extremely repetitive calls to .find and .substr
+Prior method of using an index was extremely long and unreadable, despite working just fine
+*/
+
 // Function definition for parse function, Task E2
 void Roster::parse(string studentData)
 {
-    int start = 0;
-    int end = studentData.find(',');
 
-    string studentID = studentData.substr(start, end - start);
+    string studentID;
+    string firstName;
+    string lastName;
+    string emailAddress;
+    string degreeProgramString;
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    string firstName = studentData.substr(start, end - start);
+    int age;
+    int daysInCourse1;
+    int daysInCourse2;
+    int daysInCourse3;
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    string lastName = studentData.substr(start, end - start);
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    string emailAddress = studentData.substr(start, end - start);
+    stringstream ss(studentData);
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    int age = std::stoi(studentData.substr(start, end - start));
+    getline(ss, studentID, ',');
+    getline(ss, firstName, ',');
+    getline(ss, lastName, ',');  
+    getline(ss, emailAddress, ',');
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    int daysInCourse1 = std::stoi(studentData.substr(start, end - start));
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    int daysInCourse2 = std::stoi(studentData.substr(start, end - start));
+    age = stoi((getline(ss, studentID, ','), studentID));
+    daysInCourse1 = stoi((getline(ss, studentID, ','), studentID));
+    daysInCourse2 = stoi((getline(ss, studentID, ','), studentID));
+    daysInCourse3 = stoi((getline(ss, studentID, ','), studentID));
 
-    start = end + 1;
-    end = studentData.find(',', start);
-    int daysInCourse3 = std::stoi(studentData.substr(start, end - start));
-
-    start = end + 1;
-    string degreeProgramString = studentData.substr(start);
-
+    getline(ss, degreeProgramString); 
     DegreeProgram degreeProgram;
     if (degreeProgramString == "SECURITY")
     {
@@ -90,11 +90,13 @@ void Roster::parse(string studentData)
     }
     else
     {
-        cout << "Not a valid Degree Program" << endl;
+        cout << "Not a valid Degree Program: " << degreeProgramString << endl;
+        return;
     }
 
     add(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeProgram);
 }
+
 
 // Adds a student to the roster, Task E2
 void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram)
